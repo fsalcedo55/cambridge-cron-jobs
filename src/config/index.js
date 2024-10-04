bda92720f9beb3c3a252c907eed8516ad7e31043
fsalcedo55 <fsalcedo55@hotmail.com>
2024-10-04 00:16:01 -0400

    fix email address concatenation issue

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ export const teacherEmails = [
   process.env.TEACHER_EMAIL_2,
   process.env.TEACHER_EMAIL_3,
   process.env.TEACHER_EMAIL_4,
-]
+].filter(Boolean)
 
 module.exports = {
   email: {
@@ -15,7 +15,7 @@ module.exports = {
     pass: process.env.EMAIL_PASS,
   },
   teacherEmails,
-  calendars: ["primary", teacherEmails],
+  calendars: ["primary", ...teacherEmails],
   reminderWindow: 2 * 60 * 60 * 1000, // 2 hours in milliseconds
   google: {
     clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const ReminderJob = require("./jobs/reminderJob")
 const reminderJob = new ReminderJob()
 
 // Schedule the reminder job to run every 15 minutes
-cron.schedule("*/15 * * * *", async () => {
+cron.schedule("* * * * *", async () => {
   console.log("Running reminder job...")
   await reminderJob.run()
 })
